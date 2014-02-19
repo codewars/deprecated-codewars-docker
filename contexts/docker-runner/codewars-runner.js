638c70bc8f50eb0dc793cf5f7913b2fd2a887100
Ryan Hewitt <rthewitt.devel@gmail.com>
2014-02-19 14:41:02 -0800

    manual end

--- contexts/docker-runner/codewars-runner.js
+++ contexts/docker-runner/codewars-runner.js
@@ -200,6 +200,11 @@ var ConfigureDocker = function(config){
                 cb(null, client); 
             });
         });
+
+        setTimeout(function() { 
+            self.instrument('MANUALLY ENDING CLIENT');
+            client.end();
+        }, 5000);
     }
 
     return { createRunner: _makeRunner }
